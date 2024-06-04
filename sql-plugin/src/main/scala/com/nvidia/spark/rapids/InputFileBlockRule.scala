64cfae3790f8e050028c1ff92cbd5544281b80f2
liurenjie1024 <liurenjie2008@gmail.com>
2024-06-04 15:44:41 +0800

    Fix

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/InputFileBlockRule.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/InputFileBlockRule.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2021-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,54 +33,68 @@ object InputFileBlockRule {
   private type PlanMeta = SparkPlanMeta[SparkPlan]
 
   def apply(plan: PlanMeta): Unit = {
-    // key: the SparkPlanMeta where has the first input_file_xxx expression
-    // value: an array of the SparkPlanMeta chain [SparkPlan (with first input_file_xxx), FileScan)
-    val resultOps = mutable.LinkedHashMap[PlanMeta, ArrayBuffer[PlanMeta]]()
-    recursivelyResolve(plan, None, resultOps)
+    // Cache the collected plans in range [SparkPlan(with first input_file_xxx), FileScan).
+    // key: the first plan meta containing an input_file_xxx expression.
+    // value: the plan metas in range [SparkPlan(with first input_file_xxx), FileScan)
+    val fallbackPlans = mutable.LinkedHashMap[PlanMeta, ArrayBuffer[PlanMeta]]()
+    collectPlans(plan, fallbackPlans)
 
     // If we've found some chains, we should prevent the transition.
-    resultOps.foreach { case (_, metas) =>
+    fallbackPlans.foreach { case (_, metas) =>
       metas.foreach(_.willNotWorkOnGpu("GPU plans may get incorrect file name" +
         ", or file start or file length from a CPU scan"))
     }
   }
 
-  /**
-   * Recursively apply the rule on the plan
-   * @param plan the plan to be resolved.
-   * @param key  the SparkPlanMeta with the first input_file_xxx
-   * @param resultOps the found SparkPlan chain
-   */
-  private def recursivelyResolve(plan: PlanMeta, key: Option[PlanMeta],
-      resultOps: mutable.LinkedHashMap[PlanMeta, ArrayBuffer[PlanMeta]]): Unit = {
-    plan.wrapped match {
-      case _: ShuffleExchangeLike => // Exchange will invalid the input_file_xxx
-        key.map(p => resultOps.remove(p)) // Remove the chain from Map
-        plan.childPlans.foreach(p => recursivelyResolve(p, None, resultOps))
-      case _: FileSourceScanExec | _: BatchScanExec =>
-        if (plan.canThisBeReplaced) { // FileScan can be replaced
-          key.map(p => resultOps.remove(p)) // Remove the chain from Map
-        }
-      case _: BroadcastExchangeLike =>
-        // noop: Don't go any further, the file info cannot come from a broadcast.
-      case _: LeafExecNode => // We've reached the LeafNode but without any FileScan
-        key.map(p => resultOps.remove(p)) // Remove the chain from Map
-      case _ =>
-        val newKey = if (key.isDefined) {
-          // The node is in the middle of chain [SparkPlan with input_file_xxx, FileScan)
-          resultOps.getOrElseUpdate(key.get, new ArrayBuffer[PlanMeta]) += plan
-          key
-        } else { // There is no parent node who has input_file_xxx
-          if (hasInputFileExpression(plan.wrapped)) {
-            // Current node has input_file_xxx. Mark it as the first node with input_file_xxx
-            resultOps.getOrElseUpdate(plan, new ArrayBuffer[PlanMeta]) += plan
-            Some(plan)
-          } else {
-            None
-          }
-        }
-        plan.childPlans.foreach(p => recursivelyResolve(p, newKey, resultOps))
+  private def collectPlans(
+      planMeta: PlanMeta,
+      fallbackPlans: mutable.LinkedHashMap[PlanMeta, ArrayBuffer[PlanMeta]]): Unit = {
+    if (hasInputFileExpression(planMeta.wrapped)) {
+      // Catch a plan that has an "input_file_xxx", then it becomes a key for the
+      // following collecting.
+      // 1) Go into children to collect plans for this key.
+      var hasAnyCpuFileScan = false
+      planMeta.childPlans.foreach { p =>
+        hasAnyCpuFileScan = hasAnyCpuFileScan || collectPlansWithKey(p, planMeta, fallbackPlans)
+      }
+      // 2) Handle the current plan itself.
+      // Cache the current plan iff any child has a CPU FileScan and itself will run on GPU.
+      if (hasAnyCpuFileScan && planMeta.canThisBeReplaced) {
+        fallbackPlans.getOrElseUpdate(planMeta, new ArrayBuffer[PlanMeta]) += planMeta
+      }
+    } else {
+      // Go into children to search a node that has input_file_xxx.
+      planMeta.childPlans.foreach(collectPlans(_, fallbackPlans))
     }
+  }
+
+  private def collectPlansWithKey(planMeta: PlanMeta, key: PlanMeta,
+      fallbackPlans: mutable.LinkedHashMap[PlanMeta, ArrayBuffer[PlanMeta]]): Boolean = {
+    var foundCpuFileScan = false
+    planMeta.wrapped match {
+      case _: ShuffleExchangeLike | _: BroadcastExchangeLike =>
+        // Exchange will invalid the input_file_xxx, so the input key is invalid.
+        // but need to go into children to look for new plans with input_file_xxx.
+        planMeta.childPlans.foreach(collectPlans(_, fallbackPlans))
+      case _: FileSourceScanExec | _: BatchScanExec =>
+        // Catch a FileScan, check if it can run on GPU, and no need to cache itself.
+        foundCpuFileScan = !planMeta.canThisBeReplaced
+      case _: LeafExecNode => // Noop, we've reached the LeafNode but find no FileScan
+      case _ =>
+        // The plan is in the middle of chain [SparkPlan with input_file_xxx, FileScan).
+        // NOTE: Here does not check if it has an input_file_xxx expression, so it is
+        // ignored because we already have a key in this search context.
+        // 1) Check its children to see if there is a child has a CPU FileScan.
+        planMeta.childPlans.foreach { p =>
+          foundCpuFileScan = foundCpuFileScan || collectPlansWithKey(p, key, fallbackPlans)
+        }
+        // 2) Cache the plan itself for later fallback iff it will run on GPU and at least
+        //    one CPU FileScan exists in its children.
+        if (foundCpuFileScan && planMeta.canThisBeReplaced) {
+          fallbackPlans.getOrElseUpdate(key, new ArrayBuffer[PlanMeta]) += planMeta
+        }
+    }
+    foundCpuFileScan
   }
 
   private def hasInputFileExpression(expr: Expression): Boolean = expr match {
