bf9b6d12fe0fefb88f3c0813d78352876198a689
liurenjie1024 <liurenjie2008@gmail.com>
2024-09-19 16:25:45 +0800

    hash aggtest 2 40 1 conf partition 1

--- integration_tests/src/main/python/spark_session.py
+++ integration_tests/src/main/python/spark_session.py
@@ -57,6 +57,7 @@ _default_conf = {
     'spark.rapids.sql.variableFloatAgg.enabled': 'false',
     'spark.sql.legacy.allowNegativeScaleOfDecimal': 'true',
     'spark.rapids.sql.shuffle.kudo.enable': 'true',
+    'spark.sql.shuffle.partitions': '1',
 }
 
 def _set_all_confs(conf):
