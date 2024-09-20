078af0496c73d66cba0af49702987e67bd7025c2
liurenjie1024 <liurenjie2008@gmail.com>
2024-09-20 11:44:50 +0800

    nested type

--- sql-plugin/src/main/java/com/nvidia/spark/rapids/GpuColumnVector.java
+++ sql-plugin/src/main/java/com/nvidia/spark/rapids/GpuColumnVector.java
@@ -532,13 +532,13 @@ public class GpuColumnVector extends GpuColumnVectorBase {
       String name = "_col_" + level + "_" + idx;
       if (dt instanceof MapType) {
         // MapType is list of struct in cudf, so need to handle it specially.
-        Schema.Builder listBuilder = builder.column(DType.LIST, name);
-        Schema.Builder structBuilder = listBuilder.column(DType.STRUCT, name + "_map");
+        Schema.Builder listBuilder = builder.addColumn(DType.LIST, name);
+        Schema.Builder structBuilder = listBuilder.addColumn(DType.STRUCT, name + "_map");
         MapType mt = (MapType) dt;
         DataType[] structChildren = {mt.keyType(), mt.valueType()};
         visit(structChildren, structBuilder, level + 1);
       } else {
-        Schema.Builder childBuilder = builder.column(GpuColumnVector.getRapidsType(dt), name);
+        Schema.Builder childBuilder = builder.addColumn(GpuColumnVector.getRapidsType(dt), name);
         if (dt instanceof ArrayType) {
           // Array (aka List)
           DataType[] childType = {((ArrayType) dt).elementType()};
@@ -552,8 +552,6 @@ public class GpuColumnVector extends GpuColumnVectorBase {
           }
           visit(childrenTypes, childBuilder, level + 1);
         }
-
-        childBuilder.build();
       }
     }
   }
